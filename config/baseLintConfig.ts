7c9415170e6825791b39ed5c5f7e573b8d6a8cd1
Pasha Zayko <pasha_zayko@shilab.com>
2025-09-09 15:00:24 -0400

    Expanding rule no-unused-vars
    Adding exception option to the rule to ignore variables starting with underscore
    
    Updating several packages to the latest versions

--- config/baseLintConfig.ts
+++ config/baseLintConfig.ts
@@ -58,6 +58,7 @@ export const eslintConfig = tseslint.config(
             '@typescript-eslint/no-unused-vars': [
                 'warn',
                 {
+                    'argsIgnorePattern': '^_',
                     'caughtErrors': 'none'
                 }
             ],
