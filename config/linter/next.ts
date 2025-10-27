36bf14a450ff5aa3b95b7ac66d37fc695514f1af
Elliot Huffman <ehuffman@elliot-labs.com>
2025-10-26 22:19:02 -0400

    Fix Next Linting
    Fix an issue where the next lint linting was not executing as expected.

--- config/linter/next.ts
+++ config/linter/next.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-import nextTypeScript from 'eslint-config-next/typescript';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -13,17 +10,12 @@ import reactHooks from 'eslint-plugin-react-hooks';
 /** Baseline configuration used for linting Next.JS projects the SHI - Lab way. */
 export const eslintConfig = defineConfig([
     ...baselineConfig,
-    ...(nextVitals as ReturnType<typeof defineConfig>),
-    ...(nextTypeScript as ReturnType<typeof defineConfig>),
+    {
+        'languageOptions': { 'globals': { ...globals.browser } },
+        'settings': { 'react': { 'version': 'detect' } }
+    },
     react.configs.flat['recommended'],
     react.configs.flat['jsx-runtime'],
     reactHooks.configs.flat.recommended,
-    {
-        'languageOptions': { 'globals': { ...globals.browser } },
-        'settings': {
-            'react': {
-                'version': 'detect'
-            }
-        }
-    }
+    ...(nextVitals as ReturnType<typeof defineConfig>),
 ]);
